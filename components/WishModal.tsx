f7de9f732f74a8ca6f95d61ba3a44a805aef6628
mskafo <scafoglierimattia@gmail.com>
2022-04-18 15:33:34 +0200

    wishlist fix

--- components/WishModal.tsx
+++ components/WishModal.tsx
@@ -27,7 +27,7 @@ const WishModal = ({
     <Transition appear show={modalOpen} as={Fragment}>
       <Dialog
         as="div"
-        className="fixed inset-0 z-10 overflow-y-auto"
+        className="fixed inset-0 z-50 overflow-y-auto"
         onClose={closeModal}
       >
         <div className="min-h-screen px-4 text-center">
