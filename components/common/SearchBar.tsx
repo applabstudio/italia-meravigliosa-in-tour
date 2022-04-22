c005604e6ac75ce090a2ef56ceef5eed6dce5271
mskafo <scafoglierimattia@gmail.com>
2022-04-22 14:35:15 +0200

    aggiunta file

--- components/common/SearchBar.tsx
+++ components/common/SearchBar.tsx
@@ -1,9 +1,10 @@
 // @ts-nocheck
 
 import { collection } from "firebase/firestore"
+import Link from "next/link"
 import React, { useEffect, useRef, useState } from "react"
 import { useCollection } from "react-firebase-hooks/firestore"
-import { FaSearch } from "react-icons/fa"
+import { FaSearch, FaTimes } from "react-icons/fa"
 import { firestore } from "../../firebase/clientApp"
 import Event from "../Event"
 
@@ -27,15 +28,39 @@ export const SearchBar = () => {
     {}
   )
 
+  const categorie = []
+
+  listaEventi?.docs?.map((doc) =>
+    doc
+      ?.data()
+      ?.categorie?.split(",")
+      .map((el) => {
+        if (!categorie.includes(el.replace(/\s/g, "").toLowerCase())) {
+          categorie.push(el.replace(/\s/g, "").toLowerCase())
+        }
+      })
+  )
+
   const [searchValue, setSearchValue] = useState("")
   const [focused, setFocused] = useState(false)
+  const [categoria, setCategoria] = useState("")
 
   const inputRef = useRef(null)
   useOutsideAlerter(inputRef, setFocused)
 
   return (
     <div ref={inputRef} className="relative z-40">
-      <div className="group flex w-full items-center rounded-full bg-gray-100">
+      <div className="group flex w-full items-center rounded-full bg-gray-100 pl-4">
+        {categoria !== "" && (
+          <button
+            onClick={() => setCategoria("")}
+            className="flex w-fit items-center space-x-1 rounded-md bg-primary-100 px-2 text-lg font-medium text-primary-600 transition duration-200 hover:bg-primary-200"
+          >
+            <span>{categoria}</span>
+            <FaTimes />
+          </button>
+        )}
+
         <input
           type="text"
           className="form-input h-full w-full border-0 bg-transparent px-4 py-3 text-gray-800 caret-primary-500 outline-none placeholder:text-gray-400 focus:border-0 focus:ring-0"
@@ -45,9 +70,9 @@ export const SearchBar = () => {
           onFocus={() => setFocused(true)}
         />
 
-        <button className="flex h-12 w-24 items-center justify-center rounded-full bg-primary-400 text-white outline-none ring-primary-200 ring-offset-2 transition duration-200 hover:scale-105 hover:bg-primary-500 focus:ring-2">
+        <span className="flex h-12 w-16 items-center justify-center text-gray-400">
           <FaSearch />
-        </button>
+        </span>
       </div>
 
       <div
@@ -55,9 +80,28 @@ export const SearchBar = () => {
           (!searchValue || !focused) && "hidden"
         } absolute top-16 left-0 z-50 max-h-96 w-full overflow-y-scroll rounded-md bg-white py-4 px-4 shadow-md transition-all`}
       >
+        <div className="grid grid-cols-2 gap-2">
+          {categorie.length > 0 &&
+            categorie.map((cat) => (
+              <button
+                onClick={() => setCategoria(cat)}
+                className="w-full rounded-md bg-primary-100 px-2 text-lg font-medium text-primary-600 transition duration-200 hover:bg-primary-200"
+              >
+                {cat}
+              </button>
+            ))}
+        </div>
+
         {listaEventi &&
         listaEventi?.docs?.filter((doc) =>
-          doc?.data()?.titolo?.match(new RegExp(searchValue, "i"))
+          doc?.data()?.titolo?.match(new RegExp(searchValue, "i")) &&
+          categoria === ""
+            ? true
+            : doc
+                ?.data()
+                ?.categorie?.split(",")
+                .map((el) => el.replace(/\s/g, "").toLowerCase())
+                .includes(categoria)
         )?.length > 0 ? (
           listaEventi?.docs
             ?.filter((doc) =>
