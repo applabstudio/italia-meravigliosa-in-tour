bda0f70ad7718bcdf04ad86faf81f3d01b5841f0
UmairMukhtar <umairmukhtar27@gmail.com>
2022-06-06 01:06:01 +0500

    more stuff

--- components/LandingComponent.tsx
+++ components/LandingComponent.tsx
@@ -354,13 +354,8 @@ const LandingComponent = ({ slug }: { slug: any }) => {
               fontStyle: "italic",
             }}
           >
-            “Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
-            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-            aliquip ex ea commodo consequat. Duis aute irure dolor in
-            reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
-            pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
-            culpa qui officia deserunt mollit anim id est laborum.”
+            “Guardare l'Italia con gli occhi dell'Artista, e trovare in ogni angolo del Bel Paese un luogo Meraviglioso. Guarda l'Italia con occhi nuovi con Italia Meravigliosa in Tour.
+”
           </p>
           <p
             style={{
