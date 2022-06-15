8c0438e05b1636e5bcab525369f492ea171f7c8f
UmairMukhtar <umairmukhtar27@gmail.com>
2022-06-16 01:01:22 +0500

    more

--- pages/eventi/[slug].tsx
+++ pages/eventi/[slug].tsx
@@ -1,7 +1,7 @@
 //@ts-nocheck
 
-import { FaHeart, FaMapMarkerAlt, FaPlus } from "react-icons/fa"
-import { useRouter } from "next/router";
+import { FaHeart, FaMapMarkerAlt, FaPlus, FaEye } from "react-icons/fa"
+import { useRouter } from "next/router"
 import Comment from "../../components/common/Comment"
 import { firestore } from "../../firebase/clientApp"
 import { useCollection } from "react-firebase-hooks/firestore"
@@ -15,7 +15,7 @@ const Evento = ({ slug }: { slug: string }) => {
     collection(firestore, "fl_content"),
     {}
   )
-  console.log("This is the date",data)
+  console.log("This is the date", data)
 
   const [eventi, setEventi] = useState<any[]>([])
   const [evento, setEvento] = useState<any>()
@@ -25,12 +25,10 @@ const Evento = ({ slug }: { slug: string }) => {
     const allEvents = []
     data?.docs.forEach((d) => {
       console.log("This is the fl scemal", d.data()._fl_meta_.schema)
-      if( d.data()._fl_meta_.schema === "evento")
-      {
+      if (d.data()._fl_meta_.schema === "evento") {
         setEventi((eventi) => [...eventi, d.data()])
         allEvents.push(d.data())
-      }
-      else{
+      } else {
         setCategorie((categorie) => [...categorie, d.data()])
       }
     })
@@ -76,158 +74,197 @@ const Evento = ({ slug }: { slug: string }) => {
       add(event)
     }
   }
-  const router = useRouter();
-  slug = router.query.slug;
-  console.log("This is props", router.query);
-  console.log("THis is the events",eventi,"This is evento",eventi?.filter((doc) => doc.slug === slug)[0])
+  const router = useRouter()
+  slug = router.query.slug
+  console.log("This is props", router.query)
+  console.log(
+    "THis is the events",
+    eventi,
+    "This is evento",
+    eventi?.filter((doc) => doc.slug === slug)[0]
+  )
   return (
-    <div style={{marginTop:100}}>
+    <div style={{ marginTop: 100 }}>
       <div className="mx-auto my-8 max-w-6xl">
-      {console.log(listaCategorie)}
-      <main className="flex w-full flex-col xl:flex-row">
-        <div className="flex w-full flex-col px-4">
-          <div className="relative h-40 w-full">
-            <img
-              src={evento?.copertina}
-              // style={{height:200}}
-              className="bannerImage h-full w-full object-cover"
-              alt={evento?.titolo}
-            />
-          </div>
-         
-          <br />
-          <br />
-          <div className="w-full bg-gray-100">
-            <p className="mt-2 ml-2 rounded-lg text-gray-500">Pubblicità</p>
-
-            <ins
-              className="adsbygoogle"
-              style={{ display: "block" }}
-              data-ad-client="ca-pub-1708355893696705"
-              data-ad-slot="9487119343"
-              data-ad-format="auto"
-              data-full-width-responsive="true"
-            ></ins>
-          </div>
-          <br />
-          <br />
-          <div className="flex flex-col items-center lg:flex-row lg:space-x-12">
-            <h4 className="mb-3 text-center text-5xl font-bold text-secondary-500 md:text-left">
-              {evento?.titolo}
-            </h4>
-
-            <button
-              onClick={() =>
-                addToWish({
-                  id: evento?.id,
-                  image: evento?.copertina,
-                  heading: evento?.titolo,
-                  location: evento?.luogo,
-                  to: `/eventi/${evento?.slug}`,
-                })
-              }
-              className="flex items-center space-x-1 rounded-md bg-secondary-100 px-4 py-2 font-semibold text-secondary-500 outline-none ring-secondary-200 ring-offset-2 transition duration-200 hover:bg-secondary-200 focus:ring-2"
-            >
-              <FaHeart /> <span>Wishlist</span>
-            </button>
-          </div>
-          <p className="mb-2 flex items-center space-x-1 text-sm text-gray-500">
-            <FaMapMarkerAlt /> <span>{evento?.luogo}</span>
-          </p>
-          <p className="text-center text-lg font-semibold text-gray-800 md:text-left">
-            {evento?.data}
-          </p>
-          <div className="mt-2 flex justify-center space-x-2 md:justify-start">
-            {listaCategorie.map((categoria) => (
-              <p className="rounded-sm bg-primary-100 px-2 font-medium text-primary-600">
-                {categoria?.titolo.toLowerCase()}
-              </p>
-            ))}
-          </div>
-          <br />
-          <hr />
-          <br />
-          <div className="grid grid-cols-1 space-y-20 lg:grid-cols-12 lg:space-y-0">
-            <div className="col-span-6 flex w-full flex-col">
-              <p
-                className="prose prose-red"
-                dangerouslySetInnerHTML={{
-                  __html: DOMPurify.sanitize(evento?.descrizione),
-                }}
+        {console.log(listaCategorie)}
+        <main className="flex w-full flex-col xl:flex-row">
+          <div className="flex w-full flex-col px-4">
+            <div className="relative h-40 w-full">
+              <img
+                src={evento?.copertina}
+                // style={{height:200}}
+                className="bannerImage h-full w-full object-cover"
+                alt={evento?.titolo}
               />
-                  <button
-              onClick={() =>{
-                window.location.href= evento.googleMap
-              }
-              }
-              style={{width:200}}
-              className="flex items-center space-x-1 rounded-md bg-primary-100 px-4 py-2 font-semibold text-primary-500 outline-none ring-primary-200 ring-offset-2 transition duration-200 hover:bg-secondary-200 focus:ring-2"
-            >
-              <FaMapMarkerAlt /> <span>Google Maps</span>
-            </button>
             </div>
-        
+            <div
+              className="mt-2 flex justify-center space-x-2 md:justify-start"
+              style={{
+                border: "1px solid gray",
+                marginBottom: 20,
+                padding: 5,
+                borderRadius: 10,
+              }}
+            >
+              <p
+                className="rounded-sm  px-2 font-medium"
+                style={{ width: "33%" }}
+              >
+                <span style={{ fontWeight: "bold", color: "blue" }}>
+                  Credit :
+                </span>{" "}
+                {" " + evento?.credit}
+              </p>
+              <p
+                className="rounded-sm  px-2 font-medium"
+                style={{ width: "33%", textAlign: "center" }}
+              >
+                <span style={{ fontWeight: "bold", color: "blue" }}>
+                  Views :
+                </span>{" "}
+                {" " + evento?.views}
+              </p>
+              <p
+                className="rounded-sm  px-2 font-medium"
+                style={{ width: "33%", textAlign: "center" }}
+              >
+                <span style={{ fontWeight: "bold", color: "blue" }}>
+                  Likes :
+                </span>{" "}
+                {" " + evento?.likes}
+              </p>
+            </div>
 
-            <div className="col-span-5 w-full lg:ml-20">
-              <h4 className="mb-4 text-xl font-semibold text-gray-800">
-                {commenti?.length < 2
-                  ? `${commenti?.length} commento`
-                  : commenti?.length < 1 || commenti?.length === undefined
-                  ? `0 commenti`
-                  : `${commenti?.length} commenti`}
+            <br />
+            <br />
+            <div className="w-full bg-gray-100">
+              <p className="mt-2 ml-2 rounded-lg text-gray-500">Pubblicità</p>
+
+              <ins
+                className="adsbygoogle"
+                style={{ display: "block" }}
+                data-ad-client="ca-pub-1708355893696705"
+                data-ad-slot="9487119343"
+                data-ad-format="auto"
+                data-full-width-responsive="true"
+              ></ins>
+            </div>
+            <br />
+            <br />
+            <div className="flex flex-col items-center lg:flex-row lg:space-x-12">
+              <h4 className="mb-3 text-center text-5xl font-bold text-secondary-500 md:text-left">
+                {evento?.titolo}
               </h4>
 
-              {commenti?.map((commento, i: number) => (
-                <React.Fragment key={commento.data().nome + i}>
-                  <Comment
-                    user={commento.data().nome}
-                    text={commento.data().testo}
-                  />
-                </React.Fragment>
-              ))}
-
-              <br />
-
-              <div className="space-y-4">
-                <p className="text-sm font-semibold text-gray-500">
-                  Lascia un commento
+              <button
+                onClick={() =>
+                  addToWish({
+                    id: evento?.id,
+                    image: evento?.copertina,
+                    heading: evento?.titolo,
+                    location: evento?.luogo,
+                    to: `/eventi/${evento?.slug}`,
+                  })
+                }
+                className="flex items-center space-x-1 rounded-md bg-secondary-100 px-4 py-2 font-semibold text-secondary-500 outline-none ring-secondary-200 ring-offset-2 transition duration-200 hover:bg-secondary-200 focus:ring-2"
+              >
+                <FaHeart /> <span>Wishlist</span>
+              </button>
+            </div>
+            <p className="mb-2 flex items-center space-x-1 text-sm text-gray-500">
+              <FaMapMarkerAlt /> <span>{evento?.luogo}</span>
+            </p>
+            <p className="text-center text-lg font-semibold text-gray-800 md:text-left">
+              {evento?.data}
+            </p>
+            <div className="mt-2 flex justify-center space-x-2 md:justify-start">
+              {listaCategorie.map((categoria) => (
+                <p className="rounded-sm bg-primary-100 px-2 font-medium text-primary-600">
+                  {categoria?.titolo.toLowerCase()}
                 </p>
-                <form>
-                  <input
-                    className="form-input mb-4 w-full rounded-lg border-gray-400 py-1 text-gray-800 caret-secondary-500 outline-none placeholder:text-gray-400 focus:border-secondary-500 focus:ring-0"
-                    type="text"
-                    placeholder="nome..."
-                    value={nome}
-                    onChange={(e) => setNome(e.target.value)}
-                  />
+              ))}
+            </div>
+            <br />
+            <hr />
+            <br />
+            <div className="grid grid-cols-1 space-y-20 lg:grid-cols-12 lg:space-y-0">
+              <div className="col-span-6 flex w-full flex-col">
+                <p
+                  className="prose prose-red"
+                  dangerouslySetInnerHTML={{
+                    __html: DOMPurify.sanitize(evento?.descrizione),
+                  }}
+                />
+                <button
+                  onClick={() => {
+                    window.location.href = evento.googleMap
+                  }}
+                  style={{ width: 200 }}
+                  className="flex items-center space-x-1 rounded-md bg-primary-100 px-4 py-2 font-semibold text-primary-500 outline-none ring-primary-200 ring-offset-2 transition duration-200 hover:bg-secondary-200 focus:ring-2"
+                >
+                  <FaMapMarkerAlt /> <span>Google Maps</span>
+                </button>
+              </div>
 
-                  <textarea
-                    rows={4}
-                    className="form-textarea mb-4 w-full rounded-lg border-gray-400 py-1 text-gray-800 caret-secondary-500 outline-none placeholder:text-gray-400 focus:border-secondary-500 focus:ring-0"
-                    placeholder="commento..."
-                    value={commento}
-                    onChange={(e) => setCommento(e.target.value)}
-                  />
+              <div className="col-span-5 w-full lg:ml-20">
+                <h4 className="mb-4 text-xl font-semibold text-gray-800">
+                  {commenti?.length < 2
+                    ? `${commenti?.length} commento`
+                    : commenti?.length < 1 || commenti?.length === undefined
+                    ? `0 commenti`
+                    : `${commenti?.length} commenti`}
+                </h4>
 
-                  <button
-                    onClick={uploadComment}
-                    className="group relative inline-block overflow-hidden rounded bg-purple-50 px-5 py-2.5 font-medium text-secondary-500 outline-none ring-secondary-500 ring-offset-2 transition-all duration-200 focus:ring-2"
-                    type="submit"
-                  >
-                    <span className="absolute top-0 left-0 mb-0 flex h-0 w-full translate-y-0 transform bg-secondary-500 opacity-90 transition-all duration-200 ease-out group-hover:h-full"></span>
-                    <span className="relative group-hover:text-white">
-                      Invia
-                    </span>
-                  </button>
-                </form>
+                {commenti?.map((commento, i: number) => (
+                  <React.Fragment key={commento.data().nome + i}>
+                    <Comment
+                      user={commento.data().nome}
+                      text={commento.data().testo}
+                    />
+                  </React.Fragment>
+                ))}
+
+                <br />
+
+                <div className="space-y-4">
+                  <p className="text-sm font-semibold text-gray-500">
+                    Lascia un commento
+                  </p>
+                  <form>
+                    <input
+                      className="form-input mb-4 w-full rounded-lg border-gray-400 py-1 text-gray-800 caret-secondary-500 outline-none placeholder:text-gray-400 focus:border-secondary-500 focus:ring-0"
+                      type="text"
+                      placeholder="nome..."
+                      value={nome}
+                      onChange={(e) => setNome(e.target.value)}
+                    />
+
+                    <textarea
+                      rows={4}
+                      className="form-textarea mb-4 w-full rounded-lg border-gray-400 py-1 text-gray-800 caret-secondary-500 outline-none placeholder:text-gray-400 focus:border-secondary-500 focus:ring-0"
+                      placeholder="commento..."
+                      value={commento}
+                      onChange={(e) => setCommento(e.target.value)}
+                    />
+
+                    <button
+                      onClick={uploadComment}
+                      className="group relative inline-block overflow-hidden rounded bg-purple-50 px-5 py-2.5 font-medium text-secondary-500 outline-none ring-secondary-500 ring-offset-2 transition-all duration-200 focus:ring-2"
+                      type="submit"
+                    >
+                      <span className="absolute top-0 left-0 mb-0 flex h-0 w-full translate-y-0 transform bg-secondary-500 opacity-90 transition-all duration-200 ease-out group-hover:h-full"></span>
+                      <span className="relative group-hover:text-white">
+                        Invia
+                      </span>
+                    </button>
+                  </form>
+                </div>
               </div>
             </div>
           </div>
-        </div>
-      </main>
+        </main>
+      </div>
     </div>
-    </div>
-    
   )
 }
 
