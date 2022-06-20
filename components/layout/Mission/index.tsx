10790fd0212c5e40eb372fa615daa608285915fd
Silver <silver.designer.developer@gmail.com>
2022-06-20 19:35:49 +0200

    updates

--- components/layout/Mission/index.tsx
+++ components/layout/Mission/index.tsx
@@ -2,48 +2,59 @@ import React from "react";
 
 const Mission = () => {
     
+    const style_MissionContainer = {
+      display: "flex", 
+      justifyContent: "center", 
+      paddingTop: 60 
+    }
+
+    const style_MissionWrapper = {
+      width: "100%",
+      background: "black",
+      height: 380,
+      maxWidth: "85rem",
+      display: "flex",
+      flexDirection: "column",
+      alignItems: "center"
+    }
+
+    const style_infoGray = {
+      fontSize: 16, 
+      fontWeight: "500", 
+      fontFamily: "Inter"
+    }
+
+    const style_headingMission = {
+      color: "white",
+      fontSize: 40,
+      paddingTop: 50,
+      paddingBottom: 10
+    }
+
+    const style_ParagraphMission = {
+      width: "80%",
+      textAlign: "justify",
+      fontFamily: "Inter",
+      fontSize: 22,
+      fontStyle: "italic"
+    }
+
+    const style_CeoParagraph = {
+      color: "white",
+      fontSize: 20,
+      fontWeight: "700",
+      fontFamily: "Lato"
+    }
+
     return (
-        <div style={{ display: "flex", justifyContent: "center", paddingTop: 60 }}>
-          <div
-            className="mx-auto max-w-7xl"
-            style={{
-              width: "100%",
-              background: "black",
-              height: 380,
-              maxWidth: "85rem",
-              display: "flex",
-              flexDirection: "column",
-              alignItems: "center",
-            }} className="text-center">
-              <p
-                style={{
-                  color: "white",
-                  fontSize: 40,
-                  paddingTop: 50,
-                  paddingBottom: 10,
-                }}> MISSION </p>
-              <p
-                className="infoGray"
-                style={{
-                  width: "80%",
-                  textAlign: "justify",
-                  fontFamily: "Inter",
-                  fontSize: 22,
-                  fontStyle: "italic",
-                }}>“Guardare l'Italia con gli occhi dell'Artista, e trovare in ogni
+        <div style={{ }}>
+          <div className="mx-auto max-w-7xl" style={style_MissionWrapper} className="text-center">
+              <p style={style_headingMission}> MISSION </p>
+              <p className="infoGray" style={style_ParagraphMission}>“Guardare l'Italia con gli occhi dell'Artista, e trovare in ogni
                 angolo del Bel Paese un luogo Meraviglioso. Guarda l'Italia con
                 occhi nuovi con Italia Meravigliosa in Tour. ”</p>
-              <p
-                style={{
-                  color: "white",
-                  fontSize: 20,
-                  fontWeight: "700",
-                  fontFamily: "Lato",
-                }}>Paolo Artista</p>
-              <p
-                className="infoGray"
-                style={{ fontSize: 16, fontWeight: "500", fontFamily: "Inter" }}>CEO & Founder Italia Meravigliosa
-              </p>
+              <p style={style_CeoParagraph}>Paolo Artista</p>
+              <p className="infoGray" style={style_infoGray}>CEO & Founder Italia Meravigliosa</p>
         </div>
       </div>
     )
