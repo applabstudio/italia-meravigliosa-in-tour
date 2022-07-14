55972de0db332f9f64042a55fbd814c8bbd4b29f
silver <silver.designer.developer@gmail.com>
2022-07-14 12:46:54 +0200

    updates

--- next.config.js
+++ next.config.js
@@ -1,15 +1,18 @@
 /** @type {import('next').NextConfig} */
 
 const withPWA = require("next-pwa")
-const runtimeCaching = require("next-pwa/cache");
 
 module.exports = withPWA({
-  reactStrictMode: false,
+  reactStrictMode: true,
   pwa: {
     dest: "public",
     register: true,
     skipWaiting: true,
-    runtimeCaching,
-		buildExcludes: [/middleware-manifest.json$/]
-  }
+  },
 })
+module.exports = {
+  images: {
+    loader: 'akamai',
+    path: '/'
+  },
+}
