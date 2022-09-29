bfde20b5395c3865507627b6c85cebe54ca63755
aidan <cutty.laidan77@gmail.com>
2022-09-29 11:37:23 -0400

    added: landing and provincia page sitemap

--- pages/sitemap.xml/index.tsx
+++ pages/sitemap.xml/index.tsx
@@ -1,15 +1,35 @@
-import { getServerSideSitemapIndex } from 'next-sitemap'
+import { getServerSideSitemapIndex, ISitemapField } from 'next-sitemap'
 import { GetServerSideProps } from 'next'
 
+const categorias = [
+  '',
+  'categoria/Acqua',
+  'categoria/Borghi',
+  'categoria/Sentieri',
+  'categoria/Monumenti',
+  'categoria/Natura',
+  'categoria/Curiosità',
+]
+
+export const getProvincia = async () => {
+  const provincias = await fetch("https://comuni-ita.herokuapp.com/api/province", {
+    method: "GET",
+  })
+  const res = await provincias.json()
+  return res
+}
+
 export const getServerSideProps: GetServerSideProps = async(ctx) => {
-  return getServerSideSitemapIndex(ctx, [
-    'https://www.italiameravigliosaintour.it.com/categoria/Acqua',
-    'https://www.italiameravigliosaintour.it.com/categoria/Borghi',
-    'https://www.italiameravigliosaintour.it.com/categoria/Sentieri',
-    'https://www.italiameravigliosaintour.it.com/categoria/Monumenti',
-    'https://www.italiameravigliosaintour.it.com/categoria/Natura',
-    'https://www.italiameravigliosaintour.it.com/categoria/Curiosità',
-  ])
+  const siteUrl = "http://italiameravigliosaintour.it/sitemap.xml";
+  const provincias: any = await getProvincia()
+  const fields: string[] = provincias?.map((data: any) => (
+    `${siteUrl}/provincia/${data.regione}`
+  ))
+  categorias.forEach(categoria => {
+    fields.push(`${siteUrl}/${categoria}`)
+  })
+
+  return getServerSideSitemapIndex(ctx, fields)
 }
 
 export default function SitemapIndex() {}
